5a8e41f960130e40266393f0880d32c78cd8b5e6
cRicateau <clementrp@bamlab.fr>
2017-08-02 20:50:40 +0200

    Remove material-ui

--- generators/app/templates/client/src/App.js
+++ generators/app/templates/client/src/App.js
@@ -2,15 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-
 import routes from './routes';
-
-const muiTheme = getMuiTheme(lightBaseTheme, {
-  fontFamily: 'Roboto, sans-serif',
-});
 
 class App extends React.Component {
 
@@ -22,9 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={this.props.store}>
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <Router history={this.props.history} routes={routes} />
-        </MuiThemeProvider>
+        <Router history={this.props.history} routes={routes} />
       </Provider>
     );
   }
--- generators/app/templates/client/src/containers/home-view/index.js
+++ generators/app/templates/client/src/containers/home-view/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { browserHistory, Link } from 'react-router';
 import { connect } from 'react-redux';
-import { FlatButton } from 'material-ui';
 import styles from './style.css';
 
 export class HomeView extends Component {
@@ -12,7 +11,7 @@ export class HomeView extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <FlatButton onClick={() => browserHistory.push('/page')}>Click me!</FlatButton>
+        <button onClick={() => browserHistory.push('/page')}>Click me!</button>
         <Link to='/page' >Or use a link!</Link>
       </div>
     );
