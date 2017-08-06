21c785fb43f71afd97e88746927ca9a429068937
Kevin <kevinj@theodo.fr>
2017-08-06 19:16:28 +0200

    Follow duck pattern for root component

---
+++ generators/app/templates/client/src/containers/root/root.component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import PropTypes from 'prop-types';
+import fr from 'react-intl/locale-data/fr';
+import frMessages from '../../translations/fr.json';
+import logo from './logo.svg';
+import './style.css';
+
+const locales = {
+  fr: frMessages,
+};
+
+addLocaleData([...fr]);
+
+export default class Root extends Component {
+  static propTypes = {
+    children: PropTypes.element,
+  };
+
+  render() {
+    return (
+      <div>
+        <IntlProvider locale='fr' messages={locales['fr']} >
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            {this.props.children}
+          </div>
+        </IntlProvider>
+      </div>
+    );
+  }
+}
---
+++ generators/app/templates/client/src/containers/root/root.container.js
@@ -0,0 +1,15 @@
+import Root from './root.component';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Root);
