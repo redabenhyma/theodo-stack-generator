7aea1edeab0316f63fab04bcf4b4eaca344ab40c
cRicateau <clementrp@bamlab.fr>
2017-08-19 15:36:45 +0200

    Rename header component to Root component

--- generators/app/templates/client/src/routes.js
+++ generators/app/templates/client/src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import { Header } from './components';
+import { Root } from './components';
 import { Home, Page } from './pages';
 
 const routes = (
-  <Route path="/" component={Header}>
+  <Route path="/" component={Root}>
     <IndexRoute component={Home} />
     <Route component={Page} path="/page" />
   </Route>
