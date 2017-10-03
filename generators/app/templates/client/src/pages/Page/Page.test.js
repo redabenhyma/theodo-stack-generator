4b09ad0025294a1e522df3e9627b1d6b63787154
cRicateau <clementrp@bamlab.fr>
2017-10-03 21:52:31 +0200

    Add setupTest file to fix jest compatibility issue with react 16

--- generators/app/templates/client/src/pages/Page/Page.test.js
+++ generators/app/templates/client/src/pages/Page/Page.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Page from './Page.component';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 let wrapper = null;
 
---
+++ generators/app/templates/client/src/setupTests.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+/*
+* This code is run before every jest test
+* See https://github.com/facebookincubator/create-react-app/issues/3206
+*/
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
