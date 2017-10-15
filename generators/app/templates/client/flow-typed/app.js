f7d7888eb88f2bdcde6cc92e08de711facc658d5
Thibaut Cheymol <thibautc@theodo.fr>
2017-10-15 23:29:23 +0200

    💎 Add flow-typed lib

---
+++ generators/app/templates/client/flow-typed/app.js
@@ -0,0 +1,40 @@
+// @flow
+/* eslint-disable no-undef, flowtype/delimiter-dangle */
+
+declare type RoutingStore = {
+  locationBeforeTransitions: {
+    pathname: string,
+    search: string,
+    hash: string,
+    action: string,
+    key?: string,
+    query: Object,
+  }
+};
+
+declare type PageStore = {
+  userAvatarUrl: string,
+  userId: string,
+};
+
+declare type Store = {
+  routing: RoutingStore,
+  page: PageStore,
+};
+
+declare type History = {
+  createHref: () => {},
+  createKey: () => {},
+  createLocation: () => {},
+  createPath: () => {},
+  getCurrentLocation: () => {},
+  go: () => {},
+  goBack: () => {},
+  goForward: () => {},
+  listen: () => {},
+  listenBefore: () => {},
+  push: () => {},
+  replace: () => {},
+  transitionTo: () => {},
+  unsubscribe: () => {},
+};
