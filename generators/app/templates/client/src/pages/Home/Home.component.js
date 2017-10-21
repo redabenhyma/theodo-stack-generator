7da7a02daddd31a62952ddde91f7b37d472371a4
Thibaut Cheymol <thibautc@theodo.fr>
2017-10-21 14:11:12 +0200

    🏗 Refacto Home component

--- generators/app/templates/client/src/pages/Home/Home.component.js
+++ generators/app/templates/client/src/pages/Home/Home.component.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { browserHistory, Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 import './style.css';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <p className="intro">
-          <FormattedMessage id="home.get-started" defaultMessage="To get started, edit" />
-          <code className="intro-code">src/App.js</code>
-          <FormattedMessage id="home.save-to-reload" />
-        </p>
-        <button onClick={() => browserHistory.push('/page')}>
-          <FormattedMessage id="home.click-me" />
-        </button>
-        <Link to="/page">
-          <FormattedMessage id="home.use-a-link" />
-        </Link>
-      </div>
-    );
-  }
-}
+const goToPage = () => {
+  browserHistory.push('/page');
+};
+
+const Home = () => (
+  <div>
+    <p className="intro">
+      <FormattedMessage id="home.get-started" defaultMessage="To get started, edit" />
+      <code className="intro-code">src/App.js</code>
+      <FormattedMessage id="home.save-to-reload" />
+    </p>
+    <button onClick={goToPage}>
+      <FormattedMessage id="home.click-me" />
+    </button>
+    <Link to="/page">
+      <FormattedMessage id="home.use-a-link" />
+    </Link>
+  </div>
+);
+
+export default Home;
