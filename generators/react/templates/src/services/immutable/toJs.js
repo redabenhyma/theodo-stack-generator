2755ac3080736061ac0574a0bcbbfa7f47cc4686
Thibaut Cheymol <thibautc@theodo.fr>
2018-02-16 22:03:52 +0100

    💎 Untype toJs.js

--- generators/react/templates/src/services/immutable/toJs.js
+++ generators/react/templates/src/services/immutable/toJs.js
@@ -1,4 +1,3 @@
-// @flow
 // This Higher Order Component transforms a component props from Immutable Data Structures to JS
 // For more information see below link
 // https://redux.js.org/docs/recipes/UsingImmutableJS.html#use-a-higher-order-component-to-convert-your-smart-components-immutablejs-props-to-your-dumb-components-javascript-props
@@ -9,13 +8,9 @@ export const toJS = WrappedComponent => wrappedComponentProps => {
   const KEY = 0;
   const VALUE = 1;
 
-  const propsJS = Object.entries(
-    wrappedComponentProps,
-  ).reduce((newProps, wrappedComponentProp) => {
+  const propsJS = Object.entries(wrappedComponentProps).reduce((newProps, wrappedComponentProp) => {
     /* eslint-disable no-param-reassign */
-    newProps[wrappedComponentProp[KEY]] = Iterable.isIterable(
-      wrappedComponentProp[VALUE],
-    )
+    newProps[wrappedComponentProp[KEY]] = Iterable.isIterable(wrappedComponentProp[VALUE])
       ? wrappedComponentProp[VALUE].toJS()
       : wrappedComponentProp[VALUE];
     /* eslint-enable */
