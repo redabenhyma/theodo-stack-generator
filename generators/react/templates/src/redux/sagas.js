6dcd084e50f6f06b58675ff1769f9e2e1a2e8b16
Thibaut Cheymol <thibautc@theodo.fr>
2018-02-16 22:07:22 +0100

    💎 Type Sagas.js

--- generators/react/templates/src/redux/sagas.js
+++ generators/react/templates/src/redux/sagas.js
@@ -1,9 +1,10 @@
 // @flow
 import { all } from 'redux-saga/effects'; // eslint-disable-line
+import type { Saga } from 'redux-saga';
 
 <% if (exampleRequired) { %>import { sagas as avatarSagas } from 'redux/Avatar';<% } %>
 
 // single entry point to start all Sagas at once
-export default function* rootSaga() {
-  yield all([<% if (exampleRequired) { %>avatarSagas()<% } %>]);
+export default function* rootSaga(): Saga<*> {
+  yield all(<% if (exampleRequired) { %>avatarSagas()<% } %>);
 }
