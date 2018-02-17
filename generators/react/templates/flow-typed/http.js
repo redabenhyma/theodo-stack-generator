c9b9ba2e6291e08391b60a1985272fb4c902b68a
Thibaut Cheymol <thibautc@theodo.fr>
2018-02-17 12:54:00 +0100

    💎 Rename HTTP types

--- generators/react/templates/flow-typed/http.js
+++ generators/react/templates/flow-typed/http.js
@@ -1,6 +1,6 @@
 // @flow
 /* eslint-disable no-undef, flowtype/delimiter-dangle */
 
-declare type Request = any;
-declare type Response = any;
+declare type RequestType = any;
+declare type ResponseType = any;
 declare type ErrorType = any;
