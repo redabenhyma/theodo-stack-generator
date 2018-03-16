e94b3961436b2d556a3dd21a1bb8150a153f572f
Nicolas Boutin <nicolasb@theodo.fr>
2018-03-16 18:45:18 +0100

    [Server] Remove ApiPlatform demo clone

--- generators/server/index.js
+++ generators/server/index.js
@@ -132,13 +132,7 @@ class StackGenerator extends Generator {
    }));
   }
 
-  _addSymfonyServer () {
-    this.spawnCommandSync('git', ['clone', 'https://github.com/api-platform/demo', './']);
-    this.spawnCommandSync('rm', ['./README.md']);
-    this.spawnCommandSync('rm', ['./CONTRIBUTING.md']);
-    this.spawnCommandSync('rm', ['./Dockerfile']);
-    this.spawnCommandSync('rm', ['./docker-compose.yml']);
-    this.spawnCommandSync('rm', ['-rf', '.git']);
+  _addComposer () {
     this.spawnCommandSync('php', ['-r', "copy('https://getcomposer.org/installer', 'composer-setup.php');"]);
     this.spawnCommandSync('php', ['-r', "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"]);
     this.spawnCommandSync('php', ['composer-setup.php']);
@@ -147,7 +141,7 @@ class StackGenerator extends Generator {
   }
 
   installProject() {
-    return this._addSymfonyServer()
+    return this._addComposer()
     .then(() => this._addConfigurationTemplates())
     .then(() => this._addDocumentation())
     .then(() => this._addProvisioningTemplates())
