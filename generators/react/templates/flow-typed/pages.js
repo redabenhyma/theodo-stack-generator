6fa850e784bfb9e86c892743a52594b9aa7fab07
Thibaut Cheymol <thibautc@theodo.fr>
2018-04-13 21:10:03 +0200

    💎 Reindent types on pages.js

--- generators/react/templates/flow-typed/pages.js
+++ generators/react/templates/flow-typed/pages.js
@@ -2,36 +2,36 @@
 /* eslint-disable no-undef, flowtype/delimiter-dangle */
 
 <% if (exampleRequired) { %>
-  declare type User = {
-    avatar_url?: string,
-  };
+declare type User = {
+  avatar_url?: string,
+};
 
-  declare type UpdateUserIdAction = {
-    type: 'Avatar/UPDATE_USER_ID',
-    payload: string,
-  };
+declare type UpdateUserIdAction = {
+  type: 'Avatar/UPDATE_USER_ID',
+  payload: string,
+};
 
-  declare type FetchUserRequestAction = {
-    type: 'Avatar/USER_FETCH_REQUEST',
-    payload: {
-      username: string,
-    },
-  };
+declare type FetchUserRequestAction = {
+  type: 'Avatar/USER_FETCH_REQUEST',
+  payload: {
+    username: string,
+  },
+};
 
-  declare type FetchUserSuccessAction = {
-    type: 'Avatar/USER_FETCH_SUCCESS',
-    payload: User,
-  };
+declare type FetchUserSuccessAction = {
+  type: 'Avatar/USER_FETCH_SUCCESS',
+  payload: User,
+};
 
-  declare type FetchUserErrorAction = {
-    type: 'Avatar/USER_FETCH_ERROR',
-    payload: string,
-  };
+declare type FetchUserErrorAction = {
+  type: 'Avatar/USER_FETCH_ERROR',
+  payload: string,
+};
 
-  declare type UserAction =
-    | UpdateUserIdAction
-    | FetchUserRequestAction
-    | FetchUserSuccessAction
-    | FetchUserErrorAction;
+declare type UserAction =
+  | UpdateUserIdAction
+  | FetchUserRequestAction
+  | FetchUserSuccessAction
+  | FetchUserErrorAction;
 <% } %>
 
